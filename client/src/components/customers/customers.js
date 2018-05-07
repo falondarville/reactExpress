9e34fcd5f4cd70e6ec33ca31596b5a0972fa7a57
Falon Darville <falondarville@gmail.com>
2018-05-06 20:48:44 -0700

    display data from express

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -7,7 +7,7 @@ class Customers extends Component {
     super();
 
     this.state = {
-      customer: []
+      customers: []
     }
   }
 
@@ -21,6 +21,12 @@ class Customers extends Component {
     return (
       <div>
         <h2>Customers</h2>
+        <ul>
+          {this.state.customers.map(customer =>
+              <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+            )}
+
+        </ul>
       </div>
     );
   }
